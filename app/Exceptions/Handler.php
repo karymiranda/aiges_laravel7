a9080f4e1ec5df90206bfc64de0b9a7844d3bbfe
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-01-15 09:58:10 -0600

    actualizacion laravel 8

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,9 @@
 
 namespace App\Exceptions;
 
-use Throwable;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
@@ -26,26 +26,16 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
+     /**
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Exception  $exception
      * @return void
      */
-    public function report(Throwable $exception)
+    public function register()
     {
-        parent::report($exception);
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Throwable $exception)
-    {
-        return parent::render($request, $exception);
-    }
+    
 }
