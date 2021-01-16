a6a42e2cc74fbade6d71339015090fddf552da61
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-01-16 08:26:46 -0600

    midificacion jenker

--- config/app.php
+++ config/app.php
@@ -65,8 +65,8 @@ return [
     |
     */
 
-   # 'timezone' => 'UTC',
-    'timezone' => 'America/El Salvador',
+    'timezone' => 'UTC',
+    #'timezone' => 'America/El Salvador',
 
     /*
     |--------------------------------------------------------------------------
