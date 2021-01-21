a9927cd5ca5bebd80e93d3af91be0970d82eb05e
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-01-20 21:37:18 -0600

    fix usuarios

--- resources/views/admin/seguridad/listausuariosactivos.blade.php
+++ resources/views/admin/seguridad/listausuariosactivos.blade.php
@@ -21,7 +21,67 @@
         </thead>
         <tbody>  
         @foreach($usuarios  as $key=>$usuario)  
-       
+        <?php if ($usuario->name!='RH0000-0'): ?>      
+          <tr>
+             <td>{{$key+1}}</td>  
+            <?php if ($usuario->empleado!=null): ?>
+              <td>{{$usuario->empleado->v_nombres .' '. $usuario->empleado->v_apellidos}}</td>
+            <?php else: ?>
+              <?php if ($usuario->estudiante!=null): ?>
+              <td>{{$usuario->estudiante->v_nombres .' '. $usuario->estudiante->v_apellidos}}</td>
+              <?php else: ?>
+                <td>{{$usuario->familiar->nombres .' '. $usuario->familiar->apellidos}}</td>
+              <?php endif ?>
+            <?php endif ?>                        
+            <td>{{$usuario->name}}</td>
+            <td>{{$usuario->email}}</td>
+            <td>
+             
+            </td>
+            <td>
+              <a href="{{route('verusuario',$usuario->id)}}" class="btn btn-primary" title="Ver"><i class="fa fa-eye"></i></a>
+              <?php if ($usuario->id==Auth::user()->id): ?>
+                <a disabled='true' class="btn btn-success"><i class="fa fa-edit"></i></a>
+                <a class="btn btn-danger" disabled='true'>
+                <i class="fa fa-close"></i>
+              </a>
+              <?php else: ?>
+                <a href="{{route('editarusuario',$usuario->id)}}" title="Editar" class="btn btn-success"><i class="fa fa-edit"></i></a>
+                <a class="btn btn-danger" data-toggle="modal" title="Deshabilitar" data-target="#usu_{{$usuario->id}}">
+                <i class="fa fa-close"></i>
+              </a>
+              <?php endif ?>
+              <div class="modal fade" id="usu_{{$usuario->id}}">
+                <div class="modal-dialog">
+                  <div class="modal-content">                      
+                    <div class="modal-header danger">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                      <h4 class="modal-title">Confirmar Deshabilitar</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p>¿Esta seguro, desea deshabiliar la cuenta del usuario <?php if ($usuario->empleado!=null): ?>
+                      {{$usuario->empleado->v_nombres .' '. $usuario->empleado->v_apellidos}}
+                    <?php else: ?>
+                      <?php if ($usuario->estudiante!=null): ?>
+                      {{$usuario->estudiante->v_nombres .' '. $usuario->estudiante->v_apellidos}}
+                      <?php else: ?>
+                        {{$usuario->familiar->nombres .' '. $usuario->familiar->apellidos}}
+                      <?php endif ?>
+                    <?php endif ?>?</p>
+                    </div>
+                    <div class="modal-footer">
+                      <form method="GET" action="{{route('eliminarusuario',$usuario->id)}}">
+                        <input type="hidden" name="id" value="{{$usuario->id}}">
+                        <input type="submit" value="Deshabilitar" class="btn btn-sm btn-danger delete-btn">                               
+                        <button type="button" class="btn btn-sm btn-default cancel-btn" data-dismiss="modal">Cancelar</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>                
+          </tr>
+            <?php endif ?> 
           @endforeach        
         </tbody>
       </table>
