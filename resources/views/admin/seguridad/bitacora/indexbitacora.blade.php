5d08b9ef210a2cb55c0467b98bd3b07f1f220878
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-01-20 20:02:16 -0600

    prueba bitacora

--- resources/views/admin/seguridad/bitacora/indexbitacora.blade.php
+++ resources/views/admin/seguridad/bitacora/indexbitacora.blade.php
@@ -19,7 +19,17 @@
                   <th>OPERACIONES REALIZADAS</th>
                 </thead>
                 <tbody>
-               
+                @foreach($bitacora as $key => $row)
+                <tr>
+                  <td>{{$key+1}}</td>
+                  <td>{{$row->created_at}}</td>
+                  <td>{{$row->bitacora_usuarios->name}}</td>
+                  <td>{{$row->usuario_nombre}}</td>
+
+                 
+
+                </tr>
+                @endforeach 
               </tbody>
             </table>
             </div>
