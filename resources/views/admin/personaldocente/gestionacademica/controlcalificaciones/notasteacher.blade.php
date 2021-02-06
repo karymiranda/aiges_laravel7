eabef1beed5a303952d40056aa0b691d8fc0e153
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-02-06 10:02:49 -0600

    vernotasdocentes

--- resources/views/admin/personaldocente/gestionacademica/controlcalificaciones/notasteacher.blade.php
+++ resources/views/admin/personaldocente/gestionacademica/controlcalificaciones/notasteacher.blade.php
@@ -63,7 +63,7 @@ $('#btnvernotas').on('click', function(e){
 var asig=$('select[name=materia]').val();
 $('#form').attr("method",'GET');
 $('#form').attr("target",'__blank');
-$('#form').attr("action",'cuadrorendimientoescolar_pdf/'+<?=$seccion->id?>+'/'+asig+'/view'); 
+$('#form').attr("action",'aiges.herokuapp/cuadrorendimientoescolar_pdf/'+<?=$seccion->id?>+'/'+asig+'/view'); 
  $('#form').submit(); 
 });
 
