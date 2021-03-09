76a48e471d16a3c0658555ce289f056b91a88d5e
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-03-09 11:21:05 -0600

    IMG CC certificados

--- app/Http/Controllers/CertificadosController.php
+++ app/Http/Controllers/CertificadosController.php
@@ -210,10 +210,10 @@ $fpdf->SetFont('Arial','', 8);
 //$fpdf->Cell(80, $this->height,strtoupper(utf8_decode($criterios[0]->competencia)), 1, 0, 'L', 1);
 
 $fpdf->Cell(80, $this->height+2,'', 1, 0, 'L', 1);
-$fpdf->Image('CC1.png',10,180,80);
+$fpdf->Image('CC1.png',10,182,80);
 
 
-$fpdf->Cell(35, $this->height,isset($conducta[$key]->criterio_1)?$conducta[$key]->criterio_1 :'-', 1, 0, 'C', 1);
+$fpdf->Cell(35, $this->height+2,isset($conducta[$key]->criterio_1)?$conducta[$key]->criterio_1 :'-', 1, 0, 'C', 1);
 
 
 if(isset($conducta[$key]->criterio_1)){
